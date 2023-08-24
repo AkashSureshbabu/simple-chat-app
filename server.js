8e166bfacd0f7fb5fa39787de1fb3874655820c3
Akash Sureshbabu <116627279+AkashSureshbabu@users.noreply.github.com>
2023-08-24 19:44:26 +0530

    Added the Emoji Function

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -9,18 +8,28 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname + "/public"));
 
+// Emoji mapping
+const emojiMap = {
+  "Hey": "👋",
+  "ThankYou": "🙏",
+  "Congratulations": "🤝",
+  "lol": "😂",
+  "Awesome": "🔥"
+};
+
 io.on('connection', (socket) => {
   console.log('user connected');
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Broadcast the received message to all connected clients
+    // Replace keywords with emojis in the message
+    const modifiedMsg = msg.replace(/\b(?:Hey|ThankYou|Congratulations|lol|Awesome)\b/g, match => emojiMap[match]);
+    io.emit('chat message', modifiedMsg); // Broadcast the received message (with emojis) to all connected clients
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
-
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
